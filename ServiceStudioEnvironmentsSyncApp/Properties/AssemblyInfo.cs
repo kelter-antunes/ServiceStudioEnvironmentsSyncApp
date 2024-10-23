99d861462b785a0b7dc311c629d2bbb03ca94460
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-10-23 17:44:32 +0100

    Update AssemblyInfo.cs

--- ServiceStudioEnvironmentsSyncApp/Properties/AssemblyInfo.cs
+++ ServiceStudioEnvironmentsSyncApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2024.10.9.*")]
+[assembly: AssemblyVersion("2024.10.23.*")]
 [assembly: AssemblyFileVersion("1.0.0")]
